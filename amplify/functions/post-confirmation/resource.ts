2dcef4c4f2cd6e37855539f940c585c85864c33d
ItumelengMadumo <stuchamadumo@gmail.com>
2026-07-23 12:24:27 +0200

    for deployment

--- amplify/functions/post-confirmation/resource.ts
+++ amplify/functions/post-confirmation/resource.ts
@@ -3,5 +3,4 @@ import { defineFunction } from '@aws-amplify/backend';
 export const postConfirmation = defineFunction({
   name: 'easyinsure-post-confirmation',
   entry: './handler.ts',
-  resourceGroupName: 'auth',
 });
